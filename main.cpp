f4105b9074a1387541cc265d6fbcf906a1fc41e2
Dmitry <dima2017.koshelev@yandex.ru>
2023-04-21 12:07:51 +0300

    1. Добавлен gitignore 2. Реализовано изменение кодировки и шрифта в консоли

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <stdio.h>//otsleg
+// #define _WIN32_WINNT 0x0A00
 #include <windows.h>
+
 using namespace std;
 
+
 int main(){
-    // 
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);  // Получить консоль
+    CONSOLE_FONT_INFOEX cfi;        // структура данных шрифта
+    cfi.cbSize = sizeof cfi;        // на чс, размер объекта типа данных CONSOLE_FONT_INFOEX
+    cfi.nFont = 0;                  // Номер шрифта
+    cfi.dwFontSize.X = 0;           // Размер шрифта
+    cfi.dwFontSize.Y = 48;          // Размер шрифта
+    cfi.FontFamily = FF_DONTCARE;   // Название шрифта
+    cfi.FontWeight = FW_NORMAL;     // Ширина символа
 
-    CONSOLE_FONT_INFOEX fontInfo;
-    GetCurrentConsoleFontEx(hConsole, TRUE, &fontInfo); // Получить текущий шрифт
+    // Функция для редактирования "Строчек-объекттов-ЧТО_ТО_ПОДОБНОЕ"
+    wcscpy(cfi.FaceName, L"Lucida Console");
+    // Установка другого шрифта и размера в текучей(GetStdHandle(STD_OUTPUT_HANDLE)) консоли
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
 
-    //   КУСОК КОДА !АТЕНШН!    wcsncpy(L"Arial Cyr", fontInfo.FaceName, LF_FACESIZE);
-    fontInfo.dwFontSize.X = 48;           // Размер (в логических единицах)
-    fontInfo.dwFontSize.X = 24;                   // Ширина символов кароче
-    fontInfo.dwFontSize.Y = 24;                  // А эт высота типа
-
-    SetCurrentConsoleFontEx(hConsole, TRUE, &fontInfo); // Установить новый
+    // Установка(Изменение) кодировки на UTF-8
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 
     string line;
     ifstream in("assets/intro.txt"); // таким макаром открываем файл чтобы прочесть
