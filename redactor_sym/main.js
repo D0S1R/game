33fa02305de7745544e82e4ad7b9e6b0921e91e7
Dmitry <dima2017.koshelev@yandex.ru>
2023-04-27 15:45:10 +0300

    1) Переосмыслено и переделано редактирование карт 2) редактирование "картинок", будет в будущем

--- redactor_sym/main.js
+++ redactor_sym/main.js
@@ -17,49 +17,79 @@ function downloadAsFile(data) {
     // a.click();
 }
 
-tinymce.init({
-    selector: "#bodyRedactor",
-    // theme: "modern",
-    // width: 680,
-    height: 650,
-    // relative_urls: false,
-    // remove_script_host: false,
-    // document_base_url: BASE_URL,
-    plugins: "",//advlist autolink link image lists charmap print preview hr anchor pagebreak searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking table contextmenu directionality emoticons paste textcolor code",
-    toolbar1: "",//undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
-    toolbar2: "",// | link unlink anchor | image media | forecolor backcolor  | print preview code ",
-    image_advtab: true,
+// tinymce.init({
+//     selector: "#bodyRedactor",
+//     // theme: "modern",
+//     // width: 680,
+//     height: 650,
+//     // relative_urls: false,
+//     // remove_script_host: false,
+//     // document_base_url: BASE_URL,
+//     plugins: "",//advlist autolink link image lists charmap print preview hr anchor pagebreak searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking table contextmenu directionality emoticons paste textcolor code",
+//     toolbar1: "",//undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
+//     toolbar2: "",// | link unlink anchor | image media | forecolor backcolor  | print preview code ",
+//     image_advtab: true,
 
-    language: "ru",
-    // valid_elements: 'a, div, p, span, img, script, style, h1, h2, h3, h4, h5, h6',
-    extended_valid_elements: "img[*],style[*],list[*],p[*],span[*],table[*],tbody[*],tr[*],td[*]",
-    // extended_valid_elements: "style[*]",
-});
+//     language: "ru",
+//     // valid_elements: 'a, div, p, span, img, script, style, h1, h2, h3, h4, h5, h6',
+//     extended_valid_elements: "img[*],style[*],list[*],p[*],span[*],table[*],tbody[*],tr[*],td[*]",
+//     // extended_valid_elements: "style[*]",
+// });
 
 
-let ID_REDACTOR = "bodyRedactor";
-// TODO: настройка шрифта в редакторе
-function settings_red() {
-    let el_parent = document.querySelector('#'+ID_REDACTOR+'_ifr').contentWindow.document.body;
-    alert(el_parent.innerHTML)
-    el_parent.style.fontSize = "10px";
-    el_parent.style.fontFamily = "Lucida Console";
+// let ID_REDACTOR = "bodyRedactor";
+// // TODO: настройка шрифта в редакторе
+// function settings_red() {
+//     let el_parent = document.querySelector('#'+ID_REDACTOR+'_ifr').contentWindow.document.body;
+//     alert(el_parent.innerHTML)
+//     el_parent.style.fontSize = "10px";
+//     el_parent.style.fontFamily = "Lucida Console";
 
-    let el_head_in_frayme = document.querySelector('#'+ID_REDACTOR+'_ifr').contentWindow.document.querySelector("head");
-    let link_style = document.createElement("link");
-    link_style.rel = "stylesheet"
-    link_style.href = "style.css"
-    el_head_in_frayme.append(link_style);
-}
+//     let el_head_in_frayme = document.querySelector('#'+ID_REDACTOR+'_ifr').contentWindow.document.querySelector("head");
+//     let link_style = document.createElement("link");
+//     link_style.rel = "stylesheet"
+//     link_style.href = "style.css"
+//     el_head_in_frayme.append(link_style);
+// }
   
-setTimeout(settings_red, 1000);
+// setTimeout(settings_red, 1000);
 
 // ИЗМЕНЕНИЕ ЦВЕТА
 let btn_colorMap = document.querySelector("#colorMap");
 
 btn_colorMap.onclick = () => {
-    let el_tr = document.createElement("tr");
-    let el_td = document.createElement("td");
+    let el_table = document.querySelector("#editColorMap");
+    el_table.style.borderSpacing = "0px";
 
+    for(i = 0; i < document.querySelector("input#cntY").value; i++){
+        let el_tr = document.createElement("tr");
+        for(j = 0; j < document.querySelector("input#cntX").value; j++){
+            let el_td = document.createElement("td");
+            el_td.style.maxWidth   = document.querySelector("input#sizeXTd").value;
+            el_td.style.maxHeight  = document.querySelector("input#sizeYTd").value;
+            
+            el_td.style.padding = "0px";
+            el_td.style.margin  = "0px";
+            
+            el_td.style.backgroundColor = "#000000";
+            el_td.style.color           = "#FFFFFF";
+
+            el_td.innerHTML = ".";
+
+            el_td.addEventListener("mouseover", editElTd);
+
+            el_tr.append(el_td);
+        }
+        el_table.append(el_tr);
+    }
     
+
 }
+
+
+function editElTd(e){
+    if(e.which == 1){
+        e.currentTarget.style.backgroundColor = document.querySelector("input#backColorSym").value;
+    }
+}
+
