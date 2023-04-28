b83dd1d481e2a056b50a99e89c60a223e535fdb5
Dmitry <dima2017.koshelev@yandex.ru>
2023-04-28 11:01:19 +0300

    создание и редактирование карт завершено

--- redactor_sym/main.js
+++ redactor_sym/main.js
@@ -1,20 +1,95 @@
-// создание "джейсон структуры, с инфой"
-let text = JSON.stringify({hello:'example'});
-// вызов функции, которая создаст и скачает на комп клиента файл
-downloadAsFile(text);
+// массив цветов консоли
+let arr_num_col = [
+    "rgb(12, 12, 12)",
+    "rgb(0, 54, 218)",
+    "rgb(19, 161, 14)",
+    "rgb(58, 150, 221)",
+    "rgb(197, 16, 31)",
+    "rgb(136, 23, 152)",
+    "rgb(193, 156, 0)",
+    "rgb(204, 204, 204)",
+    "rgb(118, 118, 118)",
+    "rgb(59, 120, 255)",
+    "rgb(22, 198, 12)",
+    "rgb(97, 214, 214)",
+    "rgb(231, 72, 86)",
+    "rgb(180, 0, 158)",
+    "rgb(249, 241, 165)",
+    "rgb(242, 242, 242)",
+];
 
-// Функция автоматической загрузки файла
-function downloadAsFile(data) {
-    // к какому элементу 
-    let a = document.querySelector("a");
+let el_table;
+
+// создание "джейсон структуры, с инфой"
+// let text = JSON.stringify({hello:'example'});
+let map = "";
+let map_color = "";
+// вызов функции, которая создаст и скачает на комп клиента файл
+// downloadAsFile(text);
+
+function madeTxtMap(){
+    // el_table;
+    map = "";
+    el_table = document.querySelector("#editColorMap");
+    for(i = 0; i < el_table.children.length; i++){
+        let tr_tr = el_table.children[i];
+        for(j = 0; j < tr_tr.children.length; j++){
+            map = map + tr_tr.children[j].innerHTML;
+        }
+        map = map + "\n";
+    }
+}
+function madeTxtMapColor(){
+    // el_table;
+    map_color = "";
+    el_table = document.querySelector("#editColorMap");
+    for(i = 0; i < el_table.children.length; i++){
+        let tr_tr = el_table.children[i];
+        for(j = 0; j < tr_tr.children.length; j++){
+            // ищем номер цвета
+            for(k = 0; k < arr_num_col.length; k++){
+                if(tr_tr.children[j].style.backgroundColor == arr_num_col[k]){
+                    map_color = map_color + k + "-";
+                }
+            }
+            for(k = 0; k < arr_num_col.length; k++){
+                if(tr_tr.children[j].style.color == arr_num_col[k]){
+                    map_color = map_color + k + ";";
+                }
+            }
+        }
+        map_color = map_color + "\n";
+    }
+}
+
+let a_map = document.querySelector("a#map_txt");
+a_map.onclick = () => {
+
+    madeTxtMap();
+
     // Создаем файл
-    let file = new Blob([data], {type: 'application/json'});
+    let file_map = new Blob([map], {type: 'application/json'});
+    
     // Привязываем к ссылке, или вшиваем в ссылку, объект "файл"
-    a.href = URL.createObjectURL(file);
+    a_map.href = URL.createObjectURL(file_map);
+
     // название файла
-    a.download = "example.txt";
-    // кликаем программно по ссылке, для автоматической загрузки
-    // a.click();
+    a_map.download = document.querySelector("input#nameMap").value + ".txt";
+}
+
+let a_map_color = document.querySelector("a#map_color_txt");
+a_map_color.onclick = () => {
+
+    madeTxtMapColor();
+
+    // Создаем файл
+    let file_map_color  = new Blob([map_color], {type: 'application/json'});
+    
+    // Привязываем к ссылке, или вшиваем в ссылку, объект "файл"
+    a_map_color.href = URL.createObjectURL(file_map_color);
+
+    // название файла
+    a_map_color.download = document.querySelector("input#nameMap").value + "_color.txt";
 }
 
 // tinymce.init({
@@ -58,7 +133,7 @@ function downloadAsFile(data) {
 let btn_colorMap = document.querySelector("#colorMap");
 
 btn_colorMap.onclick = () => {
-    let el_table = document.querySelector("#editColorMap");
+    el_table = document.querySelector("#editColorMap");
     el_table.style.borderSpacing = "0px";
     el_table.style.fontSize = document.querySelector("input#fontSize").value;
 
@@ -72,12 +147,12 @@ btn_colorMap.onclick = () => {
             el_td.style.padding = "0px";
             el_td.style.margin  = "0px";
             
-            el_td.style.backgroundColor = "#000000";
-            el_td.style.color           = "#FFFFFF";
+            el_td.style.backgroundColor = "rgb(12, 12, 12)";
+            el_td.style.color           = "rgb(204, 204, 204)";
 
             el_td.innerHTML = ".";
 
-            el_td.addEventListener("mousemove", editElTd);
+            el_td.addEventListener("mouseover", editElTd);
 
             el_tr.append(el_td);
         }
