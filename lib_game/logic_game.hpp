55866f4439ce7fd1183ad5214cceace1f9d8e8f0
D0S1R <doshirak688@gmail.com>
2023-05-25 15:06:19 +0300

    вывод карты сделан

--- lib_game/logic_game.hpp
+++ lib_game/logic_game.hpp
@@ -1,5 +1,18 @@
 #include "core.hpp"
 
 void Start_game(){
-    cout << "██▓▒░☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼`⌂ÇæÆ¢£¥₧ƒªº¿⌐¬½¼¡«»│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙⋅√ⁿ²■";
+while(true){
+    string line;
+        ifstream test_map("assets/maps/map1.txt");
+        if (test_map.is_open())
+        {
+            while (getline(test_map, line))
+            {
+                cout << line << endl;
+            }
+        }
+        test_map.close();
+        Sleep(10);
+        // SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+    }
 }
