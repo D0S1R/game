69b118174690ae404a51a06aaae0d2b8ac32292b
D0S1R <doshirak688@gmail.com>
2023-06-05 11:37:59 +0300

    коллизия для сундука но ходьба не работат

--- lib_game/player.hpp
+++ lib_game/player.hpp
@@ -37,7 +37,7 @@ public:
     }
     bool Collision(int X, int Y, vector <string> map_out){
         // проверка колизии перед игроком, если есть то мы стоим (возвращаем 1), если нет (возвращаем 0)
-        if(map_out[Y][X] == '#'){
+        if(map_out[Y][X] == '#' || map_out[Y][X] == '0'){
             return true;
         }
         else{
